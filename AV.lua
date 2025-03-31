38d85ad0b224c6aac2f7f4acdd541cbf9b09dcb4
Jaras <daermlo346@gmail.com>
2025-03-31 23:49:40 +0700

    Update AV.lua

--- AV.lua
+++ AV.lua
@@ -1,14 +1,2 @@
-getgenv().Configuration = {
-    Enabled = true,
-    RedeemCode = true,
-    SummonLegendary = true,
-    EquipBest = true,
-    AutoClaims = {
-        ['Achievement'] = true,
-        ['Milestones'] = false, -- // รับเควสเลเวล
-        ['Battlepas'] = true,
-        ['Quests'] = true,
-    },
-};
 getgenv().key = 'd59b3694-111e-4ed8-9046-e661c9910158'
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
