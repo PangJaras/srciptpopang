356a038664ff6037cbccbcb221860747aa9881b6
Jaras <daermlo346@gmail.com>
2025-04-03 23:33:56 +0700

    Update AV.lua

--- AV.lua
+++ AV.lua
@@ -1,3 +1,2 @@
-getgenv().Key = "MARU-UQU3P-B1V0-RQ1U-NPO3L-3JICP"
-getgenv().id = "760440516156063766"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MasterPClient/main/Private.lua"))()
+getgenv().key = {89956,76677,64607,15549,76535,154564}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
