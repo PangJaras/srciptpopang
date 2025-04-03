00a157cf112ecbd8a17a57157dc51366f9cb498f
Jaras <daermlo346@gmail.com>
2025-04-03 23:32:08 +0700

    Update AV.lua

--- AV.lua
+++ AV.lua
@@ -1,16 +1,3 @@
-if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
-getgenv().Script_Mode = "Kaitun_Script"
-_G.VanguardSettings = {
-    ['ClaimDaily'] = true,
-    ['ClaimQuests'] = true,
-    ['ClaimAchievement'] = true,
-    ['ClaimBattlepass'] = true,
-    ['Webhooks'] = {
-        ['Enabled'] = true,
-        ['Url'] = ''
-    }
-}
 getgenv().Key = "MARU-UQU3P-B1V0-RQ1U-NPO3L-3JICP"
 getgenv().id = "760440516156063766"
-getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MasterPClient/main/Private.lua"))()
