8dad66f4bec1d5578f893207b9a5640c5b14a8b3
Jaras <daermlo346@gmail.com>
2025-04-04 21:28:25 +0700

    Update AV.lua

--- AV.lua
+++ AV.lua
@@ -1,15 +1,14 @@
-if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
-getgenv().Script_Mode = "Kaitun_Script"
-_G.VanguardSettings = {
-    ['ClaimDaily'] = true,
-    ['ClaimQuests'] = true,
-    ['ClaimAchievement'] = true,
-    ['ClaimBattlepass'] = true,
-    ['Webhooks'] = {
-        ['Enabled'] = true,
-        ['Url'] = ''
-    }
-}
-(getgenv()).key = "MARUAZWEZQZTHH8I25ACMEVKMP";
-(getgenv()).id = "760440516156063766";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
+getgenv().Configuration = {
+    Enabled = true,
+    RedeemCode = true,
+    SummonLegendary = true,
+    EquipBest = true,
+    AutoClaims = {
+        ['Achievement'] = true,
+        ['Milestones'] = true, -- // รับเควสเลเวล
+        ['Battlepas'] = true,
+        ['Quests'] = true,
+    },
+};
+getgenv().key = {89956,76677,64607,15549,76535,154564}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
