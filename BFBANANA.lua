8ec22684ab6bc8a5e483cb3bdec80f1688cfd98b
Jaras <daermlo346@gmail.com>
2025-04-09 01:37:14 +0700

    Update BFBANANA.lua

--- BFBANANA.lua
+++ BFBANANA.lua
@@ -35,12 +35,12 @@ getgenv().Key = "a0cd575e213ea442da0daea7"
             ["Fragment"] = 50000,
         },
         ["Auto Chat"] = {
-            ["Enabled"] = false,
-            ["Text"] = "",
+            ["Enabled"] = true,
+            ["Text"] = "hey bro5555",
         },
         ["Auto Summon Rip Indra"] = true, --- auto buy haki and craft haki legendary 
         ["Select Hop"] = { -- 70% will have it
-            ["Hop Server If Have Player Near"] = true, 
+            ["Hop Server If Have Player Near"] = false, 
             ["Hop Find Rip Indra Get Valkyrie Helm or Get Tushita"] = true, 
             ["Hop Find Dough King Get Mirror Fractal"] = true,
             ["Hop Find Raids Castle [CDK]"] = true,
@@ -51,7 +51,7 @@ getgenv().Key = "a0cd575e213ea442da0daea7"
         },
         ["Farm Mastery"] = {
             ["Melee"] = false,
-            ["Sword"] = true,
+            ["Sword"] = false,
         },
         ["Buy Haki"] = {
             ["Enhancement"] = false,
