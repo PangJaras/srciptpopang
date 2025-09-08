2438cb08f2d97e6ef7d2218016afc3ef34105c88
Jaras <daermlo346@gmail.com>
2025-09-09 02:56:15 +0700

    Create Bz.lua

---
+++ Bz.lua
@@ -0,0 +1,6 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players
+repeat task.wait() until game.Players.LocalPlayer
+repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+script_key="DDzxYfSGGQeOgSCMaupQXQXuqdTmycPm";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7588dbfaad66bed7ea5e339650f903cd.lua"))()
