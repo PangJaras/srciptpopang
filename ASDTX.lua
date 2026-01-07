294a1dae332ce90685ef5ba42d65bb673872813e
Jaras <daermlo346@gmail.com>
2026-01-07 15:08:12 +0700

    Delete ASDTX.lua

--- ASDTX.lua
+++
@@ -1,4 +0,0 @@
-repeat wait() until game:IsLoaded()
-wait(5)
-script_key="GIfniInCjxsgNlAEyaQmwuMvzFwepjlK";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2a15f4a97e3a10f1a10dbb55c265b1f0.lua"))()
